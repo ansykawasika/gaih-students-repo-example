63a3604d29b06f7aeec1bbf986e8aab7d2101934
ansykawasika <46032737+ansykawasika@users.noreply.github.com>
2021-02-21 23:11:22 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,22 @@
-#Explain your work
+import random
+matrix = []
+matrix1 = []
+matrix2 = []
+matrix3 = []
 
-#Question 1
-for x in range(a):
-	print(a)
+while len(matrix) < 9:                      # 9 asal elemanli dizi oluştur
+    rasgele = random.randint(0,100)         # 0-100 arası rasgele sayi sec
+    if rasgele > 1:
+        for i in range(2,rasgele):
+            if (rasgele % i) == 0:
+                break
+        else:
+            matrix.append(rasgele)
+
+matrix1 = matrix[0:3]                       # 3 luk parcalara ayir
+matrix2 = matrix[3:6]
+matrix3 = matrix[6:9]
+matrix = [matrix1] + [matrix2] + [matrix3]  # 3x3 luk matrix olustur
+
+for i in matrix:
+    print(i)
